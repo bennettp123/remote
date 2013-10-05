79ee21833c3df2341fe0a191b6ddea9ffbb470cd
Bennett Perkins <whatever@bennettp123.com>
2013-10-05 00:06:56 -0700

    Erm, main.c is missing

---
+++ src/main.c
@@ -0,0 +1,90 @@
+
+#include "pebble_os.h"
+#include "pebble_app.h"
+#include "pebble_fonts.h"
+
+
+#define MY_UUID { 0x92, 0xF5, 0xA5, 0xB3, 0x92, 0x68, 0x48, 0x5B, 0x82, 0xE5, 0xE0, 0xDF, 0xB0, 0x3F, 0xAA, 0x5C }
+PBL_APP_INFO_SIMPLE(MY_UUID, "Remote", "bennettp123", 1 /* App version */);
+
+ 
+Window window;
+
+TextLayer textLayer;
+
+
+// Modify these common button handlers
+
+void up_single_click_handler(ClickRecognizerRef recognizer, Window *window) {
+  (void)recognizer;
+  (void)window;
+
+  text_layer_set_text(&textLayer, "Up!");
+}
+
+
+void down_single_click_handler(ClickRecognizerRef recognizer, Window *window) {
+  (void)recognizer;
+  (void)window;
+
+  text_layer_set_text(&textLayer, "Down!");
+}
+
+
+void select_single_click_handler(ClickRecognizerRef recognizer, Window *window) {
+  (void)recognizer;
+  (void)window;
+
+  text_layer_set_text(&textLayer, "Select!");
+}
+
+
+void select_long_click_handler(ClickRecognizerRef recognizer, Window *window) {
+  (void)recognizer;
+  (void)window;
+
+  text_layer_set_text(&textLayer, "Long!");
+}
+
+
+// This usually won't need to be modified
+
+void click_config_provider(ClickConfig **config, Window *window) {
+  (void)window;
+
+  config[BUTTON_ID_SELECT]->click.handler = (ClickHandler) select_single_click_handler;
+
+  config[BUTTON_ID_SELECT]->long_click.handler = (ClickHandler) select_long_click_handler;
+
+  config[BUTTON_ID_UP]->click.handler = (ClickHandler) up_single_click_handler;
+  config[BUTTON_ID_UP]->click.repeat_interval_ms = 100;
+
+  config[BUTTON_ID_DOWN]->click.handler = (ClickHandler) down_single_click_handler;
+  config[BUTTON_ID_DOWN]->click.repeat_interval_ms = 100;
+}
+
+
+// Standard app initialisation
+
+void handle_init(AppContextRef ctx) {
+  (void)ctx;
+
+  window_init(&window, "Remote");
+  window_stack_push(&window, true /* Animated */);
+
+  text_layer_init(&textLayer, window.layer.frame);
+  text_layer_set_text(&textLayer, "Remote");
+  text_layer_set_font(&textLayer, fonts_get_system_font(FONT_KEY_GOTHAM_30_BLACK));
+  layer_add_child(&window.layer, &textLayer.layer);
+
+  // Attach our desired button functionality
+  window_set_click_config_provider(&window, (ClickConfigProvider) click_config_provider);
+}
+
+
+void pbl_main(void *params) {
+  PebbleAppHandlers handlers = {
+    .init_handler = &handle_init
+  };
+  app_event_loop(params, &handlers);
+}
